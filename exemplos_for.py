6d3b06ddb1a5130fe1143537da7b83e2b8c0605e
Kelly <metamorfosedaya@gmail.com>
2021-06-15 22:37:57 -0300

    teste github

---
+++ exemplos_for.py
@@ -0,0 +1,14 @@
+# While: enquanto uma condição for verdadeira, repita:
+
+senha = 'Blue123'
+
+entrada = input('Digite uma senha: ')
+
+erros = 0
+
+While senha != entrada:
+    erro += 1
+    entrada = input('Senha incorreta! \ndigite novamente: ')
+
+    print('Bem Vindo!')
+
