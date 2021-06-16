5210380314825d733c091500aa44cee71fb09bad
Kelly <metamorfosedaya@gmail.com>
2021-06-15 22:50:29 -0300

    github

---
+++ exercícios.py
@@ -0,0 +1,11 @@
+#04 - Desenvolva um código em que o usuário vai entrar vários números e no final vai apresentar a soma deles (o usuário vai dizer quantos números serão informados antes de começar)05 - Faça um programa que mostre na tela uma contagem regressiva para o estouro de fogos de artifício, indo de 10 até 0, com uma pausa de 1 segundo entre eles.06 - 
+#Escreva um programa onde o usuário digita uma frase e essa frase retorna sem nenhuma vogal.
+
+soma = 0
+
+quantidade_Numeros = int(input('Quantos números você pretende somar? ')
+
+for i in range('quantidade_Numeros'):
+    soma = soma + int(input('qual o número da vez '))
+
+print('A soma é:', soma)
